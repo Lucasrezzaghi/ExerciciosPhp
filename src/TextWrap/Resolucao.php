e70a958b30bc273072eecf9eeb24ad58e7236908
Lucasrezzaghi <lbrezzaghi@gmail.com>
2020-12-02 19:23:29 +0000

    consertando erros

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -26,7 +26,6 @@ class Resolucao implements TextWrapInterface {
 	public function textWrap(string $text, int $length): array {
 		$palavras = '';
 		$ans = array();
-		$espacos = 0;
 		$words = explode(" ", $text);
 		$n = sizeof($words);
 		$palavras .= $words[0];
@@ -36,7 +35,7 @@ class Resolucao implements TextWrapInterface {
 			return [NULL];
 		}
 		for ($i = 1; $i < $n; $i++){
-			$sum = mb_strlen($words[$i]) + $sum;
+			$sum = mb_strlen($words[$i]) + $sum - 1;
 			if ( mb_strlen($palavras) <= $length){
 				if ($sum <= $length){
 					$palavras .= ' ' . $words[$i];
