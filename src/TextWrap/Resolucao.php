7ada8e77dd478c226aa71fa197d5bfdb4b157575
Lucasrezzaghi <lbrezzaghi@gmail.com>
2020-12-03 09:31:55 +0000

    consertando erros

--- src/TextWrap/Resolucao.php
+++ src/TextWrap/Resolucao.php
@@ -35,7 +35,7 @@ class Resolucao implements TextWrapInterface {
 					$sum = mb_strlen($words[$i]) + $sum + 1;
 					if ( mb_strlen($string) <= $length){
 						if ($sum <= $length){
-							if(!isset($words[$i])){
+							if(!isset($words[0])){
 								$string .= ' ' . $words[$i];
 							}
 						}
